7985a7daf1005de4b65275a316adb482da99e30b
evinaanderson <evinalykoln@gmail.com>
2025-12-18 20:20:49 +0100

    mon avant drnier commit

--- merchex/merchex/settings.py
+++ merchex/merchex/settings.py
@@ -31,13 +31,13 @@ DATABASES = {
 }
 STATIC_URL = 'static/'
 
-# STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-# STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
-
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 
 # Quick-start development settings - unsuitable for production
